d3fcd0ea90971782813ed72965800b58e277803b
Morven Lewis-Everley <morven@i-lateral.com>
2016-05-20 10:47:32 +0100

    Alter subsite extension to work with latest SS

--- code/extensions/SubsiteCatalogueExtension.php
+++ code/extensions/SubsiteCatalogueExtension.php
@@ -32,40 +32,37 @@ class SubsiteCatalogueExtension extends DataExtension
         if ($dataQuery->getQueryParam('Subsite.filter') === false) {
             return;
         }
-
-        // Don't run on delete queries, since they are always tied to
-        // a specific ID.
-        if ($query->getDelete()) {
+        
+        // If you're querying by ID, ignore the sub-site - this is a bit ugly...
+        // if(!$query->where || (strpos($query->where[0], ".\"ID\" = ") === false && strpos($query->where[0], ".`ID` = ") === false && strpos($query->where[0], ".ID = ") === false && strpos($query->where[0], "ID = ") !== 0)) {
+        if ($query->filtersOnID()) {
             return;
         }
 
-        // If you're querying by ID, ignore the sub-site - this is a bit ugly...
-        // if(!$query->where || (strpos($query->where[0], ".\"ID\" = ") === false && strpos($query->where[0], ".`ID` = ") === false && strpos($query->where[0], ".ID = ") === false && strpos($query->where[0], "ID = ") !== 0)) {
-        if (!$query->where || (!preg_match('/\.(\'|"|`|)ID(\'|"|`|)( ?)=/', $query->where[0]))) {
-            if (Subsite::$force_subsite) {
-                $subsiteID = Subsite::$force_subsite;
-            } else {
-                /*if($context = DataObject::context_obj()) $subsiteID = (int)$context->SubsiteID;
-                else */$subsiteID = (int)Subsite::currentSubsiteID();
-            }
+        if (Subsite::$force_subsite) {
+            $subsiteID = Subsite::$force_subsite;
+        } else {
+            /*if($context = DataObject::context_obj()) $subsiteID = (int)$context->SubsiteID;
+            else */$subsiteID = (int)Subsite::currentSubsiteID();
+        }
 
-            // The foreach is an ugly way of getting the first key :-)
-            foreach ($query->getFrom() as $tableName => $info) {
-                // The tableName should be SiteTree or SiteTree_Live...
-                if (strpos($tableName, $this->owner->ClassName) === false) {
-                    break;
-                }
-                $query->addWhere("\"$tableName\".\"SubsiteID\" IN ($subsiteID)");
+        // The foreach is an ugly way of getting the first key :-)
+        foreach ($query->getFrom() as $tableName => $info) {
+            // The tableName should be SiteTree or SiteTree_Live...
+            if (strpos($tableName, 'SiteTree') === false) {
                 break;
             }
+            $query->addWhere("\"$tableName\".\"SubsiteID\" IN ($subsiteID)");
+            break;
         }
     }
-
+    
     public function onBeforeWrite()
     {
         if (!$this->owner->ID && !$this->owner->SubsiteID) {
             $this->owner->SubsiteID = Subsite::currentSubsiteID();
         }
+        
         parent::onBeforeWrite();
     }
 }
