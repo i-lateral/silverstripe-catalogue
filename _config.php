2d212b7de5035c68fb57abdbefd3202bec5f642e
Morven Lewis-Everley <morven@i-lateral.com>
2017-11-21 16:19:50 +0000

    Update config

--- _config.php
+++ _config.php
@@ -1,17 +1,1 @@
 <?php
-
-// If subsites is installed
-if(class_exists('Subsite')) {
-    CatalogueProduct::add_extension('SubsiteCatalogueExtension');
-    CatalogueCategory::add_extension('SubsiteCatalogueExtension');
-    TaxRate::add_extension('SubsiteCatalogueExtension');
-    CatalogueAdmin::add_extension('SubsiteMenuExtension');
-}
-
-// Setup google sitemaps
-$catalogue_enabled = Catalogue::config()->enable_frontend;
-
-if($catalogue_enabled && class_exists("GoogleSitemap")) {
-    GoogleSitemap::register_dataobject('CatalogueProduct');
-    GoogleSitemap::register_dataobject('CatalogueCategory');
-}
