5be0ffef7e033d78db5b89123c1c00985ca87890
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-10-05 14:29:06 +0100

    Add shic for SS3.7 support

--- _config.php
+++ _config.php
@@ -12,6 +12,12 @@ if(class_exists('Subsite')) {
 $catalogue_enabled = Catalogue::config()->enable_frontend;
 
 if($catalogue_enabled && class_exists("GoogleSitemap")) {
+    // Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+    // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+    if (!class_exists('SS_Object')) {
+        class_alias('Object', 'SS_Object');
+    }
+
     GoogleSitemap::register_dataobject('CatalogueProduct');
     GoogleSitemap::register_dataobject('CatalogueCategory');
 }
